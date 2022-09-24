c6033bfcf21e95b5d5c0ccd8d5b9bb8e0af86d5d
Brad Tayan <brad@creepydate.io>
2022-09-23 22:28:20 -1000

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'./public/**/*.html',
+		'./src/**/*.{astro,md,js,jsx,svelte,ts,tsx,vue}',
+		'../../libs/**/*.{astro,md,js,jsx,svelte,ts,tsx,vue}',
+	  ],
+	theme: {
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '976px',
+			xl: '1440px',
+		  },
+		extend: {},
+	},
+	plugins: [],
+}
